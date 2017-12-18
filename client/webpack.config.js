0f2e571a877913cff83708e10a11c2767bfa8377
ng0g0 <hristov.gv@gmail.com>
2017-12-18 14:20:49 +0200

    Webpack update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,34 +12,21 @@ const config = {
   module: {
     loaders: [{
       exclude: /node_modules/,
-      test: /\.(js|jsx)$/,
+      test: /\.js)$/,
       loader: 'babel',
     },
       {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('css!sass'),
+        test: /\.css$/, 
+        loader: 'style-loader!css-loader'
       }],
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './',
   },
-  plugins: [
-    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-      output: { comments: false },
-      mangle: false,
-      sourcemap: false,
-      minimize: true,
-      mangle: { except: ['$super', '$', 'exports', 'require', '$q', '$ocLazyLoad'] },
-    }),
-    new ExtractTextPlugin('src/public/stylesheets/app.css', {
-      allChunks: true,
-    }),
-  ],
+  stats: {
+        colors: true
+    }
 };
 
 module.exports = config;
