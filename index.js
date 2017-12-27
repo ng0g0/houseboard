ed97f1cfa91a758626e4552a5fe5c3193b47686a
ng0g0 <hristov.gv@gmail.com>
2017-12-27 19:57:31 +0200

    blabla6

--- index.js
+++ index.js
@@ -1,73 +1,26 @@
 var express = require("express");
-var bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    router = require('./router'),
+    config = require('./config/main');
 //var mongodb = require("mongodb");
 ///var ObjectID = mongodb.ObjectID;
 
 //var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-//var db;
-
-// Connect to the database before starting the application server.
-//mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-//  if (err) {
-//    console.log(err);
-//    process.exit(1);
-//  }
-
-  // Save database object from the callback for reuse.
-//  db = database;
-//  console.log("Database connection ready");
+//process.env.NODE_ENV = 'production';
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+  var server = app.listen(config.api_port, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
 //});
 
 // CONTACTS API ROUTES BELOW
-
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
-
-
-/*
-
-const express = require('express'),
-  app = express(),
-  bodyParser = require('body-parser'),
-//  logger = require('morgan'),
-  router = require('./router'),
-  config = require('./config/main');
-  
-//process.env.NODE_ENV = 'production';
-  
-  
-let server;  
-console.log(`Your ENV:"${process.env.npm_package_run}".`);
-//process.env
-//if (process.env.NODE_ENV != config.test_env) {
-  server = app.listen(config.api_port);
-//  console.log(`Your server is running on port ${config.api_port}.`);
-//} else{
-//  server = app.listen(config.test_api_port);
-  console.log(`Your server is ${process.env.npm_package_run} and running on port ${config.api_port}.`);
-//}
-
-
-
-// Serve static files from the React app
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-//app.use(logger('dev')); // Log requests to API using morgan
-
-// Put all API endpoints under '/api'
 
 app.use((req, res, next) => {
    var clinet_address = config.app_url + ':' + config.client_port;	
@@ -78,17 +31,15 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
-});   
+});
+
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
 
 
-
-
-//app.get('*', function(req, res) {
-//  res.sendFile(path.join( __dirname, '/client/src/index.html'));
-//});
-
-// necessary for testing
-
+/*
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -98,6 +49,6 @@ app.use((req, res, next) => {
 //});
 */
 
-//router(app);
+router(app);
 
-//module.exports = server;
+module.exports = server;
