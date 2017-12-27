e6b4ff457f309a3b524857d849722c76a5a542e1
ng0g0 <hristov.gv@gmail.com>
2017-12-27 20:19:03 +0200

    bla10

--- index.js
+++ index.js
@@ -1,54 +1,100 @@
 var express = require("express");
-    bodyParser = require("body-parser");
-    //router = require('./router'),
-  //  config = require('./config/main');
-//var mongodb = require("mongodb");
-///var ObjectID = mongodb.ObjectID;
-
-//var CONTACTS_COLLECTION = "contacts";
+var bodyParser = require("body-parser");
+const pgp = require('pg-promise')(/*options*/);
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+var prod_connectionString = 'postgres://rrfcffhsalqnly:6b596e4fd06c6a808f1c99ddec492ff92320bc77b3b6d208125bfe1a3b0a555e@ec2-54-247-177-33,eu-west-1,compute,amazonaws,com:5432/d24mflhkuq27nd';
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 
-//process.env.NODE_ENV = 'production';
+console.log(prod_connectionString);
+const db = pgp(prod_connectionString);
+  console.log("Database connection ready");
 
   // Initialize the app.
   var server = app.listen(80, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
-//});
 
 // CONTACTS API ROUTES BELOW
-/*
-app.use((req, res, next) => {
-   var clinet_address = config.app_url + ':' + config.client_port;	
-  // console.log(`Your ENV:"${clinet_address}".`);
-  //res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.header('Access-Control-Allow-Origin', clinet_address);
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-*/
+
+// Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
 }
 
+/*  "/api/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ 
 
-/*
+app.get("/api/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-//app.get('*', (req, res) => {
-  //res.sendFile(path.join(__dirname+'/client/www/index.html'));
-//  res.sendFile(path.join(__dirname+'/www/index.html'));
-//});
+app.post("/api/contacts", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  }
+
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
 */
 
-//router(app);
+/*  "/api/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ 
 
-//module.exports = server;
+app.get("/api/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contact");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+app.put("/api/contacts/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update contact");
+    } else {
+      updateDoc._id = req.params.id;
+      res.status(200).json(updateDoc);
+    }
+  });
+});
+
+app.delete("/api/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete contact");
+    } else {
+      res.status(200).json(req.params.id);
+    }
+  });
+});
+*/
