1710ee2fa729c2d549dfc2d769ca90fb2d208ea0
ng0g0 <hristov.gv@gmail.com>
2018-01-02 15:07:29 +0200

    proxy2

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,7 +5,7 @@ import { STATIC_ERROR, FETCH_USER, SEND_CONTACT_FORM } from './types';
 export const API_URL = '/api';
 //export const API_URL = 'http://houseboard.herokuapp.com:5000/api';
 //export const CLIENT_ROOT_URL = 'http://localhost:3000';
-//export const CLIENT_ROOT_URL = 'http://houseboard.herokuapp.com';
+export const CLIENT_ROOT_URL = 'https://houseboard.herokuapp.com';
 
 //= ===============================
 // Utility actions
