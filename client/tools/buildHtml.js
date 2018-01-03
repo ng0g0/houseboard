3553767bc40507c8045580a82349f807cc93d240
ng0g0 <hristov.gv@gmail.com>
2018-01-03 13:27:09 +0200

    Build Update

--- client/tools/buildHtml.js
+++ client/tools/buildHtml.js
@@ -1,23 +1,18 @@
 import fs from 'fs';
-import cheerio from 'cheerio';
 import colors from 'colors';
 
 /*eslint-disable no-console */
 
-fs.readFile('public/index.html', 'utf8', (err, markup) => {
-  if (err) {
-    return console.log(err);
+var copydir = require('copy-dir');
+
+copydir('./public', './build', function(err){
+  if(err){
+    console.log(err);
+  } else {
+    console.log('HTML and CSS copied'.green); 
   }
-
-  const $ = cheerio.load(markup);
-
-  // since a separate spreadsheet is only utilized for the production build, need to dynamically add this here.
-  $('head').prepend('<link rel="stylesheet" href="styles.css">');
-
-  fs.writeFile('build/index.html', $.html(), 'utf8', function (err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('index.html written to /build'.green);
-  });
 });
+
+
+
+
